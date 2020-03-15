c4d150b1adb4e1a02a8af28bd6c217ea287a6d35
JimenaMV <jimenamanzanares.v@gmail.com>
2020-03-14 23:47:24 -0600

    Menu y mapa

--- src/data.js
+++ src/data.js
@@ -1,57 +1,16 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-<<<<<<< HEAD
  import data from './data/worldbank/worldbank.js';
-=======
-import data from './data/worldbank/worldbank.js';
-<<<<<<< HEAD
->>>>>>> 3eb3dba2c732d36b9f1eae1063404a906983d109
-=======
-export const example=()=>{return "example";};
-// export const newArrayIndicators = data.filter( item => {
-//    return item.indicatorCode;
-// });
->>>>>>> 238c4e34cc8eb5e3995b1897892b054e6dc02893
 
-// export const newArrayIndicators = (data) => {
-//    data.PER.indicators.map(item => console.log(item.indicators))
-// };
 
-<<<<<<< HEAD
-export const example = () => {
-  return 'example';
-};
-<<<<<<< HEAD
- console.log (data);
-=======
- 
->>>>>>> 3eb3dba2c732d36b9f1eae1063404a906983d109
-=======
+// export const example=()=>{return "example";};
+
 // newArrayIndicators(data);
 
 // export const newArrayIndicators = function(){
 //    data.PER.indicators.map(function(indicator) {
-//       console.log(newArrayIndicators);
+//       console.l og(newArrayIndicators);
 //    })
 // }
 
-
-// export function newArrayIndicatorsPeru(data){
-
-// //    let filtered = data.PER.indicators.filter(i=>i.indicatorCode==="SE.PRM.TENR.FE")
-// //    console.log(filtered)
-// // }
-//    console.log(peruFiltered)
-//    return data.PER.indicators.filter(
-//       item => item.indicatorCode==="SE.PRM.TENR.FE"
-//    )
-// }
-
-//newArrayIndicators(data)
 
 
 // export function newArrayIndicators(data){
@@ -62,12 +21,39 @@ export const example = () => {
 
 // newArrayIndicators(indicator);
 
-let braArrayIndicators = data.BRA.indicators.filter(indicators=>{
-   return indicators.indicatorCode.includes("SE.PRM.TENR.FE") | (indicators.indicatorCode.includes("SE.ENR.TERT.FM.ZS")) | (indicators.indicatorCode.includes("SE.ENR.PRIM.FM.ZS")) | (indicators.indicatorCode.includes("SE.ENR.SECO.FM.Z")) 
-})
- console.log(braArrayIndicators); 
+// export const braArrayIndicators = (data) => {
+//   console.log('data', data);
+//   data.BRA.indicators.filter(indicator=>{
+//     indicator.indicatorCode.includes("SE.PRM.TENR.FE") || indicator.indicatorCode.includes("SE.ENR.TERT.FM.ZS")  || indicator.indicatorCode.includes("SE.ENR.PRIM.FM.ZS") || indicator.indicatorCode.includes("SE.ENR.SECO.FM.Z") 
+//   })
+// }
 
- let perArrayIndicators = data.PER.indicators.filter(indicators=>{
+
+export const braArrayIndicators = (data) => {
+  console.log('data', data);
+  data.BRA.indicators.filter(tal)
+}
+// export const braArrayIndicators = () => {
+//   console.log('data', data);
+//   data.BRA.indicators.filter( indicator => indicator.indicatorCode === "SE.PRM.TENR.FE")}
+
+function tal(elem) {
+  return 
+}
+
+
+export const filtro = (data) => {
+  data.BRA.indicators.forEach((indicator) => {
+    if(indicator.indicatorCode.includes("SE.PRM.TENR.FE")) {
+      console.log(indicator);
+    }
+  })
+}
+
+// // filtro();
+//  braArrayIndicators(); 
+
+ let perA rrayIndicators = data.PER.indicators.filter(indicators=>{
    return indicators.indicatorCode.includes("SE.PRM.TENR.FE") | (indicators.indicatorCode.includes("SE.ENR.TERT.FM.ZS")) | (indicators.indicatorCode.includes("SE.ENR.PRIM.FM.ZS")) | (indicators.indicatorCode.includes("SE.ENR.SECO.FM.Z")) 
 })
  console.log(perArrayIndicators); 
@@ -83,4 +69,3 @@ let braArrayIndicators = data.BRA.indicators.filter(indicators=>{
    return indicators.indicatorCode.includes("SE.PRM.TENR.FE") | (indicators.indicatorCode.includes("SE.ENR.TERT.FM.ZS")) | (indicators.indicatorCode.includes("SE.ENR.PRIM.FM.ZS")) | (indicators.indicatorCode.includes("SE.ENR.SECO.FM.Z")) 
 })
  console.log(mexArrayIndicators); 
->>>>>>> 238c4e34cc8eb5e3995b1897892b054e6dc02893
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import data from './data/worldbank/worldbank.js';
-import {example} from './data.js';
-// import { newArrayIndicators } from './data.js';
+// import {example} from './data.js';
+import { filtro} from './data.js';
 
-// console.log (newArrayIndicators());
+console.log(filtro(data));
+
+// console.log (braArrayIndicators());
 // console.log(data.BRA.indicators);
 // console.log (indicatorCountry);
 // console.log(data);
